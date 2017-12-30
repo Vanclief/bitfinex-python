2b340900a585c397ef60737c69c6d3e1e79ed083
Franco Valencia <franco.avalencia@gmail.com>
2017-12-30 11:09:34 -0600

    Bumped up version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='bitfinexpy',
-    version='0.1.1',
+    version='0.1.2',
     description='Unofficial Python3 library for the Bitfinex API',
     long_description=readme,
     author='Franco Valencia',
